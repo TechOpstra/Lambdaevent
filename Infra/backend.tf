1c557559e05b603d6fed0f04e4fe3f3f54fe6213
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-24 09:31:37 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.0.0"
   backend "s3" {
-    bucket = "openproject-bucket1"  # Replace with your unique S3 bucket name
-    key    = "path/image/my-bucket2/statefile/terraform.tfstate"  # Path inside the bucket to store the state
+    bucket = ""techopsbucket123"  # Replace with your unique S3 bucket name
+    key    = "vpcec2/terraform.tfstate10" # Path inside the bucket to store the state
     region = "us-east-1"  # Replace with your desired AWS region
     encrypt = true  # Enable encryption of the state file
-   
+    dynamodb_table = "demo_table123"
   }
 }
